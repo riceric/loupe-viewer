c6285ffe533681f8d6ddd50c691584c46bfa24fa
Hui, Eric <EHui@columbia.com>
2018-05-14 22:51:56 -0700

    Add support for Vimeo, Scene7 and YouTube

--- app.js
+++ app.js
@@ -3,13 +3,19 @@
  * Require JQuery, SlickJS
  */
 var Loupe = (function() {
+    // Module settings
+    var settings = {
+        videoSlide: $('.loupe__slide--video'),
+        navItem: $('.loupe-nav__item'),
+
+    }
+
     /* TODO figure out how to store Scene7 parameters */
-    // var dimParamThumbs = '?wid=80&hei=80';   // Try reusing default for thumbs, too
     var dimParamDefault = '?wid=675&hei=675';
     var dimParamHQ = '?wid=1280&hei=1280';
 
     /**
-     * Activate 'magnifying glass' effect. 
+     * Activate 'magnifying glass' effect.
      * Pre-requisite: HTML must follow a template
      * @access public
      * @param {String} selector DOM selector containing loupe images
@@ -28,32 +34,14 @@ var Loupe = (function() {
             e.offsetY ? offsetY = e.offsetY : offsetX = e.touches[0].pageX;
             x = offsetX/magnified.offsetWidth*100;
             y = offsetY/magnified.offsetHeight*100;
-            magnified.style.backgroundPosition = x + '% ' + y + '%';     
+            magnified.style.backgroundPosition = x + '% ' + y + '%';
           }
         },
         mouseleave: function() {
           $(this).removeClass('is-active');
         }
-        
-      }); 
-    };
 
-    /**
-     * 
-     */
-    var scriptLoaded = function(src) {
-        var duplicateFound = false;
-        // Only load script if it is not already loaded
-        
-        var loadedScripts = document.getElementsByTagName("script");
-        for (var i = 0; i < loadedScripts.length; i++){
-            if (loadedScripts[i].src == src) { // Make sure script isn't already loaded
-                duplicateFound = true;
-                break;
-            }     
-        }
-
-        return duplicateFound;
+      });
     };
 
     /**
@@ -62,56 +50,19 @@ var Loupe = (function() {
      * @param {String} imgData JSON data containing image URLs
      * @param {String} slidesSelector Selector that contains carousel slickjs slides
      * @param {String} navSelector Selector that contains carousel slickjs nav
-     * 
+     *
      */
     var loadCarouselViewers = function(imgData, slidesSelector, navSelector) {
         var $slides = $(slidesSelector);
         var $nav = $(navSelector);
-        var $videoSlide = $('.loupe__slide--video');
+        var $videoSlide = settings.videoSlide;
+        var videoID = imgData['video'].data;
+
         var scrollOnDesktop = true; // Placeholder for MHW / SOR functionality
         var player; // iframe containing video player
 
         /* TODO: Figure out where to store default N/A image */
-        var imgNA = "/on/demandware.static/Sites-Columbia_US-Site/-/default/dw3ad2cfbf/images/noimagelarge.png";
-
-        // Setup carousel slides
-        for (var i in imgData["images"]) {
-            var slide = "<div class='loupe__slide'><figure class='loupe' data-lgimg='"+imgData.images[i]+dimParamHQ+"'><div id='loupe__lens'><img src='"+imgData.images[i]+dimParamDefault+"' class='loupe__img'></div></figure></div>";
-            var navSlide = "<div class='loupe-nav__item'><img src='"+imgData.images[i]+dimParamDefault+"' class='loupe-nav__img' /></div>";
-            $slides.append(slide);
-            $nav.append(navSlide);
-        }
-        // Append video slide to main carousel and nav carousel, 
-        if (imgData["video"].data != '') {
-            if (imgData["video"].host == 'youtube') {
-                $slides.append("<div class='loupe__slide loupe__slide--video'><img src='https://img.youtube.com/vi/"+ imgData["video"].data +"/hqdefault.jpg' /></div>");
-                $nav.append("<div class='loupe-nav__item loupe-nav__item--video'><img src='https://img.youtube.com/vi/"+ imgData["video"].data +"/hqdefault.jpg' class='loupe-nav__img' /></div>")
-            }
-            if (imgData["video"].host == 'vimeo') {
-                // A Vimeo API call is required to get the thumbnail URL
-                var vimeoID = imgData["video"].data;
-
-                $.getJSON('https://www.vimeo.com/api/v2/video/' + vimeoID + '.json?callback=?', {format: "json"}, function(data) {
-                    $(".thumbs").attr('src', data[0].thumbnail_large);
-                    $slides.append("<div class='loupe__slide loupe__slide--video'><img src='https://i.vimeocdn.com/video/"+ data[0].thumbnail_large +"' /></div>");
-                    $nav.append("<div class='loupe-nav__item loupe-nav__item--video'><img src='https://i.vimeocdn.com/video/"+ data[0].thumbnail_large +"' class='loupe-nav__img' /></div>")        
-                });
-            }
-            if (imgData["video"].host == 'scene7') {
-                $slides.append("<div class='loupe__slide loupe__slide--video'><img src='https://i.vimeocdn.com/video/"+ imgData["video"].data +"_640.jpg' /></div>");
-                $nav.append("<div class='loupe-nav__item loupe-nav__item--video'><img src='https://i.vimeocdn.com/video/"+ imgData["video"].data +"_640.jpg' class='loupe-nav__img' /></div>")
-            }            
-        }
-
-        // Init video viewer (draft)
-        $('.loupe-nav__item--video').on('click', function() {
-            console.log('Video slide clicked...');
-            // If video is already loaded, just play video
-            if ($videoSlide.find('#loupe-video').length < 1) {
-                console.log('YT video not found. Loading now...');
-                loadYouTubeVideo();
-            }
-        });
+        //var imgNA = "/on/demandware.static/Sites-Columbia_US-Site/-/default/dw3ad2cfbf/images/noimagelarge.png";
 
         // Requires slick.js to be loaded
         $slides.slick({
@@ -142,10 +93,55 @@ var Loupe = (function() {
                         vertical: true,
                         verticalSwiping: true,
                     }
-                }                  
-            ]               
+                }
+            ]
         });
-    };   
+
+        // Setup carousel slides
+        for (var i in imgData['images']) {
+            $slides.slick('slickAdd','<div class="loupe__slide"><figure class="loupe" data-lgimg="' + imgData.images[i] + dimParamHQ + '"><div id="loupe__lens"><img src="' + imgData.images[i] + dimParamDefault + '" class="loupe__img"></div></figure></div>');
+            $nav.slick('slickAdd','<div class="loupe-nav__item"><img src="'+ imgData.images[i] + dimParamDefault + '" class="loupe-nav__img" /></div>');
+        }
+        // Append video slide to main carousel and nav carousel,
+        if (videoID != '') {
+            if (imgData['video'].host == 'youtube') {
+                $slides.slick('slickAdd','<div class="loupe__slide loupe__slide--video"><img src="https://img.youtube.com/vi/'+ videoID +'/hqdefault.jpg" /></div>');
+                $nav.slick("slickAdd",'<div class="loupe-nav__item loupe-nav__item--video"><img src="https://img.youtube.com/vi/'+ videoID +'/hqdefault.jpg" class="loupe-nav__img" /></div>');
+
+                // Init YouTube viewer
+                $('.loupe-nav__item--video').on('click.togglePlayback', function() {
+                    // If video is already loaded, just play video
+                    if ($videoSlide.find('#loupe-video').length < 1) {
+                        loadYouTubeVideo(videoID);
+                    }
+                });
+            } else if (imgData['video'].host == 'vimeo') {
+                // If video is hosted by Vimeo, get thumbnail URL, which is different than the videoID and requires an API call
+                $.getJSON('https://www.vimeo.com/api/v2/video/' + videoID + '.json?callback=?', {format: "json"}, function(data) {
+                    $slides.slick('slickAdd','<div class="loupe__slide loupe__slide--video"><img src="https://i.vimeocdn.com/video/'+ data[0].thumbnail_large +'" /></div>');
+                    $nav.slick('slickAdd','<div class="loupe-nav__item loupe-nav__item--video"><img src="https://i.vimeocdn.com/video/'+ data[0].thumbnail_large +'" class="loupe-nav__img" /></div>');
+
+                    // Init Vimeo viewer
+                    $('.loupe-nav__item--video').on('click.togglePlayback', function() {
+                        // If video is already loaded, just play video
+                        if ($videoSlide.find('#loupe-video').length < 1) {
+                            loadVimeoVideo(videoID);
+                        }
+                    });
+                });
+            } else if (imgData['video'].host == 'scene7') {
+                $slides.slick('slickAdd','<div class="loupe__slide loupe__slide--video"><img src="https://s7d2.scene7.com/is/image/ColumbiaSportswear2/'+ videoID +'?wid=675" /></div>');
+                $nav.slick('slickAdd','<div class="loupe-nav__item loupe-nav__item--video"><img src="https://s7d2.scene7.com/is/image/ColumbiaSportswear2/'+ videoID +'?wid=675" class="loupe-nav__img" /></div>');
+
+                // Init Scene7 video viewer
+                $('.loupe-nav__item--video').on('click.togglePlayback', function() {
+                    if($videoSlide.find('#loupe-video').length < 1) {
+                        loadS7Video(videoID);
+                    }
+                });
+            }
+        }
+    };
 
     /**
      * Load YouTube video
@@ -153,75 +149,140 @@ var Loupe = (function() {
      * @param {Object} target Target DOM element
      * @return void
      */
-    var loadYouTubeVideo = function() {
-        var scriptURL = "https://www.youtube.com/iframe_api";
+    var loadYouTubeVideo = function(videoID) {
+        var scriptURL = 'https://www.youtube.com/iframe_api';
         var width = 1024;
         var height = 576;
         var siteURL = 'columbia.com';
-        var videoID = 's6Ine_o2lLM';
         var videoHTML = '<iframe id="loupe-video" type="text/html" ' +
                             'width="'+ width +'" height="'+ height +'" ' +
                             'src="http://www.youtube.com/embed/'+ videoID +
                             '?color=white&rel=0&showinfo=0&enablejsapi=1" ' +
-                            'frameborder="0">' + 
+                            'frameborder="0">' +
                         '</iframe>';
         var $target = $('.loupe__slide--video');
-  
-        console.log('loadYouTubeVideo: adding iframe to the DOM...')
+
         // Load video into slide
         $target.html(videoHTML);
 
         // Use API with existing YouTube player
-        // Only load YouTube script if it is not found
+        // Only load YouTube script if it is not found (YTPlayer defined)
         // Loads the YouTube IFrame Player API code asynchronously.
-        if (!scriptLoaded(scriptURL)) {// Add check for existing YT script
+        if (typeof YTPlayer == 'undefined') {
             var tag = document.createElement('script');
-            tag.src = "https://www.youtube.com/iframe_api";
             var firstScriptTag = document.getElementsByTagName('script')[0];
+            tag.src = scriptURL;
             firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
         }
         return false;
     }
 
     /**
+     * Load Vimeo video
+     * @param {String} videoID Vimeo Video ID
+     */
+    var loadVimeoVideo = function(videoID) {
+        var scriptURL = 'https://player.vimeo.com/api/player.js';
+        var width = 1024;
+        var height = 576;
+        var $target = $('.loupe__slide--video');
+        var videoOptions = {
+            id: videoID,
+            width: width,
+            loop: false
+        };
+
+        // Use API with existing YouTube player
+        // Only load YouTube script if it is not found (YTPlayer defined)
+        if (typeof vimeoPlayer == 'undefined') {
+            var tag = document.createElement('script');
+            var firstScriptTag = document.getElementsByTagName('script')[0];
+
+            // Wait for remote script to load before assigning event listeners
+            tag.onload = function() {
+                // Clear placeholder image before loading video
+                $target.html('');
+
+                // Create vimeo player
+                var vimeoPlayer = new Vimeo.Player($target, videoOptions);
+
+                $('.loupe-nav__item').not('.loupe-nav__item--video')
+                .on('click', function(){
+                    vimeoPlayer.pause();
+                });
+
+                $('.loupe-nav__item--video')
+                    .off('click.togglePlayback')
+                    .on('click', function() {
+                        vimeoPlayer.play();
+                });
+            };
+            tag.src = scriptURL;
+            firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+        }
+
+    }
+
+    /**
      * Load Scene7 video
      */
-    var loadS7Video = function() {
+    var loadS7Video = function(videoID) {
+        var s7ServerURL = "http://s7d2.scene7.com/is/image/";
+        var s7VideoServerURL = "http://s7d2.scene7.com/is/content/";
+        var s7basePath = "ColumbiaSportswear2/";
+        var s7contentURL = "http://s7d2.scene7.com/skins/";
+        var s7emailURL = "http://s7d2.scene7.com/s7/emailFriend";
+        var s7ConfigPath = "Scene7SharedAssets/Universal_HTML5_Video";
         var scriptURL = "https://s7d2.scene7.com/s7viewers/html5/js/VideoViewer.js";
         var duplicateFound = false;
         // Only load Scene7 script if it is not found
         // Loads the Scene7 IFrame Player API code asynchronously.
-        
-        var loadedScripts = document.getElementsByTagName("script");
-        for (var i = 0; i < loadedScripts.length; i++){
-            if (loadedScripts[i].src == scriptURL) { // Make sure script isn't already loaded
-                duplicateFound = true;
-                break;
-            }     
-        }
 
-        if (!scriptLoaded(scriptURL)) {
+        console.log("loadS7Video ("+videoID+")...");
+        if (typeof s7_videoview == 'undefined') {
+            console.log("s7VideoViewer not found...");
             var tag = document.createElement('script');
-            tag.src = scriptURL;
             var firstScriptTag = document.getElementsByTagName('script')[0];
+
+            // Append div with id='loupe-viewer' as container for s7 video
+            $('.loupe__slide--video').html('<div id="loupe-viewer"></div>');
+
+            // Initiate viewer once script is loaded
+            tag.onload = function() {
+                console.log("Init new s7VideoViewer...");
+                var s7_videoview = new s7viewers.VideoViewer({
+                    "containerId" : "loupe-viewer",
+                    "params" : {
+                        "serverurl" : s7ServerURL,
+                        "asset" : s7basePath + videoID,
+                        "contenturl": s7contentURL,
+                        "config": s7ConfigPath,
+                        "autoplay": "1",
+                        "loop": "1",
+                        "emailurl": s7emailURL,
+                        "videoserverurl": s7VideoServerURL
+                    }
+                }).init();
+
+                // Simulate click on s7playPause button
+                // Click on video nav item to play
+                $('.loupe-nav__item').not('.loupe-nav__item--video')
+                    .on('click', function(){
+                        $('#loupe-viewer_playPauseButton[selected="false"]').click();
+                });
+                // Click on non-video nav item to pause
+                $('.loupe-nav__item--video')
+                    .off('click.togglePlayback')
+                    .on('click', function() {
+                        $('#loupe-viewer_playPauseButton[selected="true"]').click();
+                });
+        
+            };
+            tag.src = scriptURL;
             firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
         }
-        
-        if (typeof s7viewers.VideoViewer != 'function') {
-            var s7_videoview = new s7viewers.VideoViewer({
-                "containerId" : "s7_videoview",
-                "params" : {
-                    "serverurl" : "http://s7d2.scene7.com/is/image/",
-                    "asset" : "ColumbiaSportswear2/sor16-web",
-                    "contenturl": "http://s7d2.scene7.com/skins/",
-                    "config": "Scene7SharedAssets/Universal_HTML5_Video", 
-                    "emailurl": "http://s7d2.scene7.com/s7/emailFriend", 
-                    "videoserverurl": "http://s7d2.scene7.com/is/content/"
-                }	
-            }).init();
-        }
     }
-    
+
     return {
         magnify: magnify,
         loadCarouselViewers: loadCarouselViewers,
@@ -229,10 +290,37 @@ var Loupe = (function() {
 
 })();
 
+/* ########## YouTube API ########### */
+/* TODO: Moving out of Loupe due to scope issues */
+// Use API with existing YouTube player
+var YTPlayer;
+var onYouTubeIframeAPIReady = function() {
+    YTPlayer = new YT.Player('loupe-video', {
+        events: {
+            'onReady': onPlayerReady,
+        }
+    });
+}
+
+// The API will call this function when the video player is ready.
+var onPlayerReady = function(event) {
+    // Setup click event that can pause videos when the iframe loses "focus"
+    $('.loupe-nav__item').not('.loupe-nav__item--video')
+        .on('click', function(){
+            YTPlayer.pauseVideo();
+    });
+
+    $('.loupe-nav__item--video')
+        .off('click.togglePlayback')
+        .on('click', function() {
+            YTPlayer.playVideo();
+    });
+}
+
 /* ########## INITIALIZATION ########## */
 $(function() {
     /* Placeholder for JSON data that should come from PDP variant
-        TODO: Change reference to json object 
+        TODO: Change reference to json object
         * N/A image URL?
         * alt text?
         * type, eg. _f, _b, a1, a2, a3, video?
@@ -261,42 +349,21 @@ $(function() {
           "host": "vimeo"
         }
     };
+    var imgData_s7 = {
+        "images": [
+            "https://s7d5.scene7.com/is/image/ColumbiaSportswear2/1792132_039_f",
+            "https://s7d5.scene7.com/is/image/ColumbiaSportswear2/1792132_039_b",
+            "https://s7d5.scene7.com/is/image/ColumbiaSportswear2/1792132_039_a1",
+            "https://s7d5.scene7.com/is/image/ColumbiaSportswear2/1792132_039_a2",
+        ],
+        "video": {
+          "data": "sor16-web",
+          "host": "scene7"
+        }
+    };
 
     // Load images for selected product variant
-    Loupe.loadCarouselViewers( imgData_vimeo, '.loupe-main', '.loupe-nav');
+    Loupe.loadCarouselViewers( imgData_s7, '.loupe-main', '.loupe-nav');
     // Initiate loupe mode on image
     Loupe.magnify( '.loupe' );
-
-    // Test Scene7Viewer
-    Loupe.loadS7Video();
-
 });
-
-/* TODO: Moving out of Loupe due to scope issues */
-// Use API with existing YouTube player
-var player;
-var onYouTubeIframeAPIReady = function() {
-    player = new YT.Player('loupe-video', {
-        events: {
-            'onReady': onPlayerReady,
-        }
-    });
-}
-
-// The API will call this function when the video player is ready.
-var onPlayerReady = function(event) {
-    // Setup click event that can pause videos when the iframe loses "focus"
-    console.log('YT onReady state detected...');
-    $('.loupe-nav__item').not('.loupe-nav__item--video')
-        .on('click', function(){
-            player.pauseVideo();
-            console.log('Pausing video...');
-    });
-
-    $('.loupe-nav__item--video')
-        .off('click', '**')
-        .on('click', function() {
-            player.playVideo();
-            console.log('Resuming playback...')
-    });
-}
